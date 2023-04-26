8f9b7f7a020e61b73ba7e5227b3db9a44be183ba
rashaAshawe <106562847+rashaAshawe@users.noreply.github.com>
2023-04-26 20:29:00 +0300

    Add files via upload

---
+++ Rasha/SudokuSE/src/sudokuse/sudokuClient.java
@@ -0,0 +1,86 @@
+
+package sudokuse;
+
+import java.io.IOException;
+  
+public class sudokuClient {
+    /**
+     * * @param args An array of command-line arguments.
+     * Displays the selected image from the server.
+     * @throws IOException If an I/O error occurs while connecting to the server or reading the response.
+     */
+     public static void main(String[] args) throws IOException {
+     // Get user input for image choice
+     
+     
+     // Send HTTP request for selected image
+     
+     
+     // Read response as image and display in JFrame
+     }}
+
+/**
+ * Partition:
+
+User input for image choice:
+a. Valid input .
+b. Invalid input (a string outside the range of available images).
+
+
+HTTP request for selected image:
+a. Successful HTTP request.
+b. Unsuccessful HTTP request due to server errors.
+c. Unsuccessful HTTP request due to network errors.
+
+Response as image:
+a. Valid response (an image is returned).
+b. Invalid response (an error message is returned).
+c. Empty response (nothing is returned).
+
+Subdomains:
+
+User input for image choice:
+a. Partition 1a: Enter a valid name within the range of available image names.
+b. Partition 1b: Enter a number outside the range of available image names.
+
+
+HTTP request for selected image:
+a. Partition 2a: Send a valid HTTP request to the server.
+b. Partition 2b: Send an HTTP request that results in server errors (e.g., 404 Not Found).
+c. Partition 2c: Send an HTTP request that results in network errors (e.g., connection timeout).
+
+Response as image:
+a. Partition 3a: Receive a valid image as the response.
+b. Partition 3b: Receive an error message as the response.
+c. Partition 3c: Receive an empty response.
+
+Test coverage:
+* The test suite cover all the subdomains
+
+Test for partition 1a: Enter a valid number within the range of available images.
+
+Expected result: The program should proceed to send an HTTP request for the selected image.
+Test for partition 1b: Enter a number outside the range of available images.
+
+
+
+Expected result: The program should display an error message indicating that the input is invalid.
+Test for partition 2a: Send a valid HTTP request to the server.
+
+Expected result: The program should receive a valid response containing the selected image.
+Test for partition 2b: Send an HTTP request that results in server errors.
+
+Expected result: The program should display an error message indicating that the server could not process the request.
+Test for partition 2c: Send an HTTP request that results in network errors.
+
+Expected result: The program should display an error message indicating that there is a problem with the network connection.
+Test for partition 3a: Receive a valid image as the response.
+
+Expected result: The program should display the selected image in a JFrame.
+Test for partition 3b: Receive an error message as the response.
+
+Expected result: The program should display an error message indicating that there is a problem with the server.
+Test for partition 3c: Receive an empty response.
+
+Expected result: The program should display an error message indicating that the response is empty.
+ */
